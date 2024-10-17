0c1ac4da5b57df99b9c78c2b48049e5294efbe9f
april-victory <apple.vika@yandex.ru>
2024-10-17 17:49:49 +0300

    fix about

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,17 +1,17 @@
-const container = document.querySelector(".about-info");
+// const container = document.querySelector(".about-info");
 
-function resizeText() {
-  const originalFontSize = 1.5;
-  let fontSize = originalFontSize;
-  container.style.fontSize = `${fontSize}rem`;
-  while (container.scrollHeight > container.clientHeight && fontSize > 0.8) {
-    fontSize -= 0.1;
-    container.style.fontSize = `${fontSize}rem`;
-  }
-}
-const resizeObserver = new ResizeObserver(() => {
-  resizeText();
-});
-resizeObserver.observe(container);
+// function resizeText() {
+//   const originalFontSize = 1.5;
+//   let fontSize = originalFontSize;
+//   container.style.fontSize = `${fontSize}rem`;
+//   while (container.scrollHeight > container.clientHeight && fontSize > 0.8) {
+//     fontSize -= 0.1;
+//     container.style.fontSize = `${fontSize}rem`;
+//   }
+// }
+// const resizeObserver = new ResizeObserver(() => {
+//   resizeText();
+// });
+// resizeObserver.observe(container);
 
-window.addEventListener("load", resizeText);
+// window.addEventListener("load", resizeText);
