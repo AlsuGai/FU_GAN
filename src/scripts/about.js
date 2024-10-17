447cd8abf5f90ae12625c48a0c45b83877ac4de2
april-victory <apple.vika@yandex.ru>
2024-10-17 18:13:24 +0300

    fix about

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,17 +1,28 @@
-// const container = document.querySelector(".about-info");
+const container = document.querySelector(".about-info");
 
-// function resizeText() {
-//   const originalFontSize = 1.5;
-//   let fontSize = originalFontSize;
-//   container.style.fontSize = `${fontSize}rem`;
-//   while (container.scrollHeight > container.clientHeight && fontSize > 0.8) {
-//     fontSize -= 0.1;
-//     container.style.fontSize = `${fontSize}rem`;
-//   }
-// }
-// const resizeObserver = new ResizeObserver(() => {
-//   resizeText();
-// });
-// resizeObserver.observe(container);
+function resizeText() {
+  const originalFontSize = 1.8;
+  let fontSize = originalFontSize;
+  container.style.fontSize = `${fontSize}rem`;
+  while (container.scrollHeight > container.clientHeight && fontSize > 0.8) {
+    fontSize -= 0.1;
+    container.style.fontSize = `${fontSize}rem`;
+  }
+}
 
-// window.addEventListener("load", resizeText);
+function shouldResizeText() {
+  return window.matchMedia("(min-width: 769px)").matches;
+}
+
+const resizeObserver = new ResizeObserver(() => {
+  if (shouldResizeText()) {
+    resizeText();
+  }
+});
+resizeObserver.observe(container);
+
+window.addEventListener("load", () => {
+  if (shouldResizeText()) {
+    resizeText();
+  }
+});
