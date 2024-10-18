1913ec62bc2fa24db8bf81150ab9401266416564
AlsuGai <alsu.gai05@mail.ru>
2024-10-18 16:48:09 +0300

    edit js

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -4,15 +4,17 @@ import auriculotherapy from "/assets/images/services/auriculotherapy.jpg";
 import acupressure from "/assets/images/services/acupressure.jpg";
 import electropuncture from "/assets/images/services/electropuncture.jpg";
 import pir from "/assets/images/services/pir.jpg";
+import dataJSON from "/assets/data/services.json";
+
 
 const btnPrev = document.getElementById("prevBtnServices");
 const btnNext = document.getElementById("nextBtnServices");
 const servicesContainer = document.getElementById("services-container");
+const data = JSON.stringify(dataJSON);
+const servicesData = JSON.parse(data);
 let id = 0;
 let currentIndex = id;
-let info = [];
 let isSwiping = false;
-
 btnNext.addEventListener("click", () => {
   if (currentIndex >= 5) {
     currentIndex = 0;
@@ -34,24 +36,20 @@ btnPrev.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  try {
-    fetch("FU_GAN/assets/data/services.json")
-      .then((res) => res.json())
-      .then((data) => {
-        info = data;
-        createService(info);
-      });
-  } catch (error) {
-    console.log(error.message);
-  }
+    
+    if(servicesData) {
+        createService(servicesData);
+    } else {
+        console.log(error.message);
+    }
 });
 
 function createService() {
   servicesContainer.innerHTML = `<div>
-            <div class=${info[currentIndex].image}>
-              <h3>${info[currentIndex].title}</h3>
+            <div class=${servicesData[currentIndex].image}>
+              <h3>${servicesData[currentIndex].title}</h3>
             </div>
-            <p>${info[currentIndex].text}</p>
+            <p>${servicesData[currentIndex].text}</p>
           </div>`;
 }
 
@@ -63,7 +61,7 @@ servicesContainer.addEventListener("touchstart", (event) => {
   startX = event.touches[0].clientX;
 });
 
-feedbackDiv.addEventListener("touchmove", (event) => {
+servicesContainer.addEventListener("touchmove", (event) => {
   if (isSwiping) return;
   const moveX = event.touches[0].clientX;
   const diffX = startX - moveX;
