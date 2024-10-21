5872ec3604a254eb5a6c7a3084e079e9a6a0adf9
april-victory <apple.vika@yandex.ru>
2024-10-22 01:06:10 +0300

    add feedbacks

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,25 +1,25 @@
-const container = document.querySelector(".about-info");
+// const container = document.querySelector(".about-info");
 
-function resizeText() {
-  const minFontSize = 1;
-  let fontSize = 1.5;
-  if (window.innerWidth > 1200) {
-    fontSize += ((window.innerWidth - 1200) / 1200) * 0.5;
-  }
+// function resizeText() {
+//   const minFontSize = 1;
+//   let fontSize = 1.5;
+//   if (window.innerWidth > 1200) {
+//     fontSize += ((window.innerWidth - 1200) / 1200) * 0.5;
+//   }
 
-  container.style.fontSize = `${fontSize}rem`;
+//   container.style.fontSize = `${fontSize}rem`;
 
-  while (
-    container.scrollHeight > container.clientHeight &&
-    fontSize > minFontSize
-  ) {
-    fontSize -= 0.2;
-    container.style.fontSize = `${fontSize}rem`;
-  }
-}
+//   while (
+//     container.scrollHeight > container.clientHeight &&
+//     fontSize > minFontSize
+//   ) {
+//     fontSize -= 0.2;
+//     container.style.fontSize = `${fontSize}rem`;
+//   }
+// }
 
-// Обработчик события изменения размера окна
-window.addEventListener("resize", resizeText);
+// // Обработчик события изменения размера окна
+// window.addEventListener("resize", resizeText);
 
-// Обработчик события загрузки страницы
-window.addEventListener("load", resizeText);
+// // Обработчик события загрузки страницы
+// window.addEventListener("load", resizeText);
